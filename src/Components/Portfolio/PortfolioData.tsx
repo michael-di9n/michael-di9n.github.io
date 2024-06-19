4e2ca4faa26927bf5fce88ce52d420ee09cb7249
Michael Di9n <165776008+michael-di9n@users.noreply.github.com>
2024-06-19 17:26:23 +1000

    portfolioddata

--- src/Components/Portfolio/PortfolioData.tsx
+++ src/Components/Portfolio/PortfolioData.tsx
@@ -11,20 +11,20 @@ export const PORTFOLIO_DATA : ProjectInfo[] = [
         "video": video,
         "image": templateImg,
         "link":  "https://www.google.com/",
-        "tools": ["Vanilla Js, Chart.js"]
+        "tools": ["Vanilla Js", "Chart.js"]
     },
     {
-        "project": "<project name>",
-        "description": "<some description",
+        "project": "WingspanGo: Bird classifier mobile app",
+        "description": "WingspanGo is a mobile bird classifier app inspired by pokemon go and wingspan board game. It uses a basic image classifier running on a raspberryPi expressjs server",
         "image": templateImg,
         "link":  "https://www.google.com/",
-        "tools": ["React.js, D3.js"]
+        "tools": ["React-Native", "Expressjs", "fastai machine learning","Python", "RasberryPi server"]
     },
     {
-        "project": "<project name>",
-        "description": "<some description",
+        "project": "Blog",
+        "description": "This is a collection of blog post detailed diffrent types of work and though processies",
         "image": templateImg,
         "link":  "https://www.google.com/",
-        "tools": ["React.js, Three.js"]
+        "tools": ["Nextjs", "PostgreSQL"]
     }
 ] 
